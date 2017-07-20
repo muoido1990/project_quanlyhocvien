0daf171f259cd30fac870e5849997666eb5365a6
MuoiDo <muoido1990@gmail.com>
2017-07-20 16:43:04 +0700

    add file

---
+++ insert.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8" />
+<style type="text/css">
+    label{
+        float: left;
+        width: 100px;
+    }
+    input{
+        margin-bottom: 5px;
+    }
+</style>
+<form action="" method="post">
+    <label>Họ và tên</label>
+    <input type="text" name="fullname" value="" />
+    <br/>
+    <label>Quê quán</label>
+    <input type="text" name="address" value="" />
+    <br/>
+    <label>Giới tính</label>
+    Nam<input type="radio" name="gender" value="1" />
+    Nữ<input type="radio" name="gender" value="2" />
+    <br/>
+    <label>&nbsp;</label>
+    <input type="submit" name="ok" value="Thêm mới" />
+</form>
